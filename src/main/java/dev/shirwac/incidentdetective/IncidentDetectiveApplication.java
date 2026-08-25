d4d85c202facfc9183e90461ff98ad37f877882c
Shirwac Abib <83682762+Sh1re-a@users.noreply.github.com>
2026-08-25 10:26:03 +0200

    backend: add Spring Boot starter

---
+++ src/main/java/dev/shirwac/incidentdetective/IncidentDetectiveApplication.java
@@ -0,0 +1,12 @@
+package dev.shirwac.incidentdetective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentDetectiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentDetectiveApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/shirwac/incidentdetective/IncidentDetectiveApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.shirwac.incidentdetective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentDetectiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
